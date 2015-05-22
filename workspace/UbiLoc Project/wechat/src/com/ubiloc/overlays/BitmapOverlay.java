a18fcaf408c5a745c151469ad8f273d91f4087d7
wuyf <811641053@qq.com>
2015-05-22 10:58:25 +0800

    增加了绘制图形图斑的功能
    增加了绘制图形图斑的功能；
    增加了一个处理图片的工具类；

---
+++ workspace/UbiLoc Project/wechat/src/com/ubiloc/overlays/BitmapOverlay.java
@@ -0,0 +1,73 @@
+package com.ubiloc.overlays;
+
+import java.util.List;
+
+import org.mapsforge.android.maps.Projection;
+import org.mapsforge.core.GeoPoint;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+
+import com.donal.wechat.R;
+import com.ubiloc.tools.BitmapTools;
+
+/**
+ * 绘制图形图层
+ * 
+ * @author crazy
+ * @Date 2015-5-20
+ */
+public class BitmapOverlay implements BaseOverlayItem {
+	/**
+	 * 图形图斑
+	 */
+	private List<BitmapOverlayItem> mOverlayItems;
+	/**
+	 * 当前图层的标识
+	 */
+	private String mkey;
+	private Context context;
+
+	public BitmapOverlay(Context context) {
+		mkey = "";
+	}
+
+	@Override
+	public void draw(Canvas canvas, Projection projection, Paint paint) {
+		if (mOverlayItems != null) {
+			paint.setColor(Color.RED);
+			paint.setAlpha(130);
+			paint.setAntiAlias(true);// 去除锯齿
+			for (BitmapOverlayItem overlayItem : mOverlayItems) {
+				GeoPoint geoPoint = overlayItem.getCoord();
+				if (geoPoint != null) {
+					Bitmap bitmap = overlayItem.getBitmap();
+					if (bitmap == null)
+						bitmap = BitmapTools.getBitmapById(context,
+								R.drawable.draw_bitmap_default);
+					Point screenPoint = new Point();
+					projection.toPixels(geoPoint, screenPoint);
+					canvas.drawBitmap(bitmap, screenPoint.x, screenPoint.y,
+							paint);
+				}
+			}
+		}
+	}
+
+	@Override
+	public String getKey() {
+		return mkey;
+	}
+
+	public void setBitmapOverlayItems(List<BitmapOverlayItem> overlayItems) {
+		this.mOverlayItems = overlayItems;
+	}
+
+	public void setKey(String key) {
+		this.mkey = key;
+	}
+}
---
+++ workspace/UbiLoc Project/wechat/src/com/ubiloc/overlays/BitmapOverlayItem.java
@@ -0,0 +1,117 @@
+package com.ubiloc.overlays;
+
+import org.mapsforge.core.GeoPoint;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+import com.ubiloc.tools.BitmapTools;
+
+/**
+ * 绘制图形图层时，每一个图斑所需包含的数据
+ * 
+ * @author crazy
+ * @Date 2015-5-22
+ */
+public class BitmapOverlayItem {
+	private Context context;
+	/**
+	 * 图形图斑的经纬度
+	 */
+	private GeoPoint mCoord;
+	/**
+	 * 图片
+	 */
+	private Bitmap mBitmap;
+
+	public BitmapOverlayItem() {
+	}
+
+	/**
+	 * 2015-5-22
+	 * 
+	 * @param coord
+	 *            经纬度
+	 * @param bitmap
+	 *            显示的图片
+	 */
+	public BitmapOverlayItem(Context context, GeoPoint coord, Bitmap bitmap) {
+		this.context = context;
+		this.mCoord = coord;
+		this.mBitmap = bitmap;
+	}
+
+	/**
+	 * 2015-5-22
+	 * 
+	 * @param coord
+	 *            经纬度
+	 * @param rId
+	 *            图片资源的id
+	 */
+	public BitmapOverlayItem(Context context, GeoPoint coord, int rId) {
+		this.context = context;
+		this.mCoord = coord;
+		if (context != null) {
+			this.mBitmap = BitmapTools.getBitmapById(context, rId);
+		}
+	}
+
+	public GeoPoint getCoord() {
+		return mCoord;
+	}
+
+	/**
+	 * 设置图片图斑对应的经纬度
+	 * 
+	 * @param mCoord
+	 * @return
+	 */
+	public BitmapOverlayItem setCoord(GeoPoint mCoord) {
+		this.mCoord = mCoord;
+		return this;
+	}
+
+	/**
+	 * 返回当前的图片资源
+	 * 
+	 * @return
+	 */
+	public Bitmap getBitmap() {
+		return mBitmap;
+	}
+
+	/**
+	 * 设置显示的图片
+	 * 
+	 * @param mBitmap
+	 *            图片资源
+	 * @return
+	 */
+	public BitmapOverlayItem setBitmap(Bitmap mBitmap) {
+		this.mBitmap = mBitmap;
+		return this;
+	}
+
+	/**
+	 * 设置显示的图片
+	 * 
+	 * @param rId
+	 *            图片资源对应的id
+	 * @return
+	 */
+	public BitmapOverlayItem setBitmap(int rId) {
+		if (context != null) {
+			this.mBitmap = BitmapTools.getBitmapById(context, rId);
+		}
+		return this;
+	}
+
+	// private Bitmap getBitmapById(Context context, int rId) {
+	// InputStream is = context.getResources().openRawResource(rId);
+	// BitmapFactory.Options opts = new BitmapFactory.Options();
+	// opts.inSampleSize = 2;
+	// Bitmap bitmap = BitmapFactory.decodeStream(is, null, opts);
+	// return bitmap;
+	// }
+}
